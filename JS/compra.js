0d083f1808a34fe3bd7373f2f16287ba7d9145c4
Stanislav2003 <pluvec03@gmail.com>
2022-01-25 11:34:30 +0100

    ok

--- JS/compra.js
+++ JS/compra.js
@@ -7,12 +7,14 @@ let correu= localStorage.getItem('correuPerson')
 btn.addEventListener("click", (e) => {
     
     if (namee== "" ) {
+      alert("registrat abans de comprar")
         window.location.href="registre.html"
         
         
       } 
     else{
       window.location.href="comprar.html"
+      alert("t'hem estafat pringat, jajaja 300 euros per aixo xd")
     }  
     });
         
